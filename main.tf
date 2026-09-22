8499305e284df9a43903c986bd2ac0124851f627
Praveen Chauhan <pravnchauhan@gmail.com>
2026-09-23 00:39:04 +0530

    rg added production1

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "production" {
+resource "azurerm_resource_group" "production1" {
   for_each = var.rgs
   name     = each.value.name
   location = each.value.location
